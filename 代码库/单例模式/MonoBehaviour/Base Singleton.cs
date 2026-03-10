590524db28a7e62666e751c0ab66181964977b00
管子 <zyg200698@gmail.com>
2026-03-10 15:53:53 +0800

    Add files via upload

--- 代码库/单例模式/MonoBehaviour/Base Singleton.cs
+++ 代码库/单例模式/MonoBehaviour/Base Singleton.cs
@@ -2,27 +2,26 @@ using UnityEngine;
 
 public class BaseSingleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-    
     static T _instance;
 
     public static T Instance
     {
         get
         {
-            if (_instance == null)
-            {
-                // 动态挂载
-                GameObject obj = new();
-                _instance = obj.AddComponent<T>();
-
-                // 设置对象名称
-                obj.name = typeof(T).Name;
-                
-                // 切换场景不销毁
-                DontDestroyOnLoad(obj);
-            }
-
             return _instance;
         }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        _instance = this as T;
+        
+        DontDestroyOnLoad(gameObject);
     }
 }
