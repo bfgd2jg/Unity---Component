eb75ca232da833de4e0e42c4ff864367ec727e42
管子 <zyg200698@gmail.com>
2026-03-10 14:14:54 +0800

    Add files via upload

--- 代码库/单例模式/Base Singleton.cs
+++ 代码库/单例模式/Base Singleton.cs
@@ -1,38 +1,27 @@
+using System;
+using System.Reflection;
 // 单例模式的基类
 // 加上 abstract 防止被 new  破坏单例唯一性
-public abstract class BaseSingleton<T> where T : class, new() // 泛型约束 T 必须是一个类且具有无参构造函数 
+public abstract class BaseSingleton<T> where T : class // 泛型约束 T 必须是一个类且具有无参构造函数 
 {
-    static T _instance;
-
-    // 通过属性访问单例实例
-    public static T Instance
+    static readonly Lazy<T> instance = new(() =>
     {
-        get
-        {
-            if (_instance == null)
-            {
-                // 使用反射创建实例 速度慢但是不需要 new() 约束
-                //_instance = System.Activator.CreateInstance<T>(); 
+        Type type = typeof(T);
 
-                // 直接使用 new 创建实例 速度快但是需要 new() 约束
-                _instance = new T();
+        ConstructorInfo info = type.GetConstructor
+        (
+            BindingFlags.Instance |
+            BindingFlags.NonPublic,
+            null,
+            Type.EmptyTypes,
+            null
+        );
 
-            }
-            return _instance;
-        }
-    }
+        if(info == null)
+            throw new Exception($"{type} 必须要有私有构造函数");
 
-    // 通过函数访问单例实例
-    public static T GetInstance()
-    {
-        if (_instance == null)
-        {
-            // 使用反射创建实例 速度慢但是不需要 new() 约束
-            //_instance = System.Activator.CreateInstance<T>(); 
+        return info.Invoke(null) as T;
+    });
 
-            // 直接使用 new 创建实例 速度快但是需要 new() 约束
-            _instance = new T();
-        }
-        return _instance;
-    }
+    public static T Instance => instance.Value;
 }
